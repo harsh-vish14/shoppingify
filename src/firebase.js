cf5baade89455bf981c9878fd30def84c8571d45
harsh-vish14 <harshkv9322@gmail.com>
2022-05-05 16:53:51 +0530

    changed the google login text on button

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyBbkDR1eiY9sdtCJey3gnS2Rhu4uVq_1IU",
+  authDomain: "shoppingify-react.firebaseapp.com",
+  projectId: "shoppingify-react",
+  storageBucket: "shoppingify-react.appspot.com",
+  messagingSenderId: "496910144590",
+  appId: "1:496910144590:web:a4c4584cbee86d31095f1f",
 };
 const firebaseApp = firebase.initializeApp(config);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
-const GoogleProvider = new firebase.auth.GoogleAuthProvider()
+const GoogleProvider = new firebase.auth.GoogleAuthProvider();
 
 export { db, auth, GoogleProvider };
